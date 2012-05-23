23782f0c4682d233bee0e47ce2a386df9b0f77c9
artem.prikhodin <artem.p@ezscratch.com>
2012-05-24 00:46:55 +0400

    removed imports from __init__.py in root

--- __init__.py
+++ __init__.py
@@ -1,10 +1,2 @@
 #coding: utf8
 
-from model.rmodel import RModel
-from model.rmodel_store import RModelStore
-from model.fields.rfield import rfield
-from model.fields.rhash import rhash
-from model.cursor import Cursor
-from model.fields.rlist import rlist
-from model.fields.rinmodel import rinmodel
-
