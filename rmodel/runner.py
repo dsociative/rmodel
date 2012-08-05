11484fdca54059712c46a4e99ebeff92f0434c68
artem.prikhodin <admin@geektech.ru>
2012-08-06 03:54:00 +0400

    teamcity integration runner.py with unittest2

--- rmodel/runner.py
+++ rmodel/runner.py
@@ -1,14 +1,30 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import unittest
+import unittest2
+import sys
+
+from teamcity.unittestpy import TeamcityTestRunner
+from teamcity import underTeamcity
 
 if __name__ == '__main__':
 
-    MASK = 'Test'
+    count = 0
+    errors = 0
+    fails = 0
 
-    runner = unittest.TextTestRunner()
-    loader = unittest.TestLoader()
+    if underTeamcity():
+        runner = TeamcityTestRunner()
+    else:
+        runner = unittest2.TextTestRunner()
+
+    loader = unittest2.TestLoader()
     suites = loader.discover('./', pattern='zt_*.py')
     for suite in suites:
-        runner.run(suite)
+        result = runner.run(suite)
+        errors += len(result.errors)
+        fails += len(result.failures)
+        count += suite.countTestCases()
+
+    if errors or fails:
+        sys.exit(1)
