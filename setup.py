a117484f68750c7195edf2dcade5f2f3b7e32ae3
dsociative <admin@geektech.ru>
2013-02-14 12:39:41 +0400

    Fixes #13 Package requirement imporves

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
+
 
 setup(name='rmodel',
       description='simple redis model',
@@ -8,4 +9,11 @@ setup(name='rmodel',
       author_email='admin@geektech.ru',
       packages=['rmodel', 'rmodel.fields', 'rmodel.models', 'rmodel.sessions'],
       package_dir={'rmodel': 'rmodel'},
-     )
+      dependency_links=[
+          "http://github.com/dsociative/ztest/tarball/master#egg=ztest-0.0.0",
+      ],
+      install_requires=[
+          'redis',
+          'ztest'
+      ]
+)
