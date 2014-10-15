e7bf534203f1a58d5c119343e82e2b2d253cb9ca
artem.prikhodin <admin@geektech.ru>
2014-10-15 04:14:26 +0400

    Update setup.py
    removed ztest from deps

--- setup.py
+++ setup.py
@@ -10,12 +10,8 @@ setup(
     author_email='admin@geektech.ru',
     packages=['rmodel', 'rmodel.fields', 'rmodel.models', 'rmodel.sessions'],
     package_dir={'rmodel': 'rmodel'},
-    dependency_links=[
-        'http://github.com/dsociative/ztest/tarball/master#egg=ztest-0.0.0',
-    ],
     install_requires=[
-        'redis',
-        'ztest'
+        'redis'
     ],
     version='0.1.2'
 )
